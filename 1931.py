93cab8d44fe5403f319ea2a0f7c9d641249c5da3
seongwon-ha <djfqks22@naver.com>
2023-06-30 15:41:40 +0900

    1931 clear

--- 1931.py
+++ 1931.py
@@ -6,16 +6,9 @@ for _ in range(n) :
     tup = (start, end)
     list.append(tup)
 
-t1 = sorted(list, key=lambda x:x[1])
+t1 = sorted(list, key=lambda x:(x[1],x[0]))
 
-for i in range (0, n-1) :
-    if t1[i][1] == t1[i+1][1] :
-        if t1[i][0] > t1[i+1][0] :
-            t = t1[i]
-            t1[i] = t1[i+1]
-            t1[i+1] = t
 
-print(t1)
 
 c = 0
 s = 0
